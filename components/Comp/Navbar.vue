f0cb83bd89f39ca480e8f400468344ae5ba41251
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-19 18:31:22 -0500

    chore: edit nav title

--- components/Comp/Navbar.vue
+++ components/Comp/Navbar.vue
@@ -37,11 +37,11 @@ beforeEach(({ name }) => {
         <span class="navbar-toggler-icon" />
       </button>
       <div class="navbar-brand ms-2 ms-lg-0 me-auto d-flex align-items-center gap-1 text-decoration-none">
-        <span width="45">SoloQ Challenge</span>
+        <span width="45">SoloQ Challenge Demo</span>
       </div>
       <div id="offcanvasNavbar" class="offcanvas offcanvas-start bg-secondary" tabindex="-1" aria-labelledby="offcanvasNavbarLabel">
         <div class="offcanvas-header px-5 pt-4 pb-0">
-          <span width="45">SoloQ Challenge</span>
+          <span width="45">SoloQ Challenge Demo</span>
           <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
         </div>
         <div class="offcanvas-body">
