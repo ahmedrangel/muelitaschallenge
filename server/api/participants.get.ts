0df4165ff2ad5a07004cddffc6c1bdc134849110
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-19 00:12:00 -0500

    chore: adjust sort

--- server/api/participants.get.ts
+++ server/api/participants.get.ts
@@ -2,6 +2,6 @@ export default defineEventHandler(async () => {
   const DB = process.env.PARTICIPANTS as any;
   const { results } = await DB.prepare("SELECT riot_name, riot_tag, lol_picture, twitch_login, twitch_display, twitch_picture, twitter, is_live, is_ingame, wins, losses, lp, elo, tier, position, position_change from participants").all();
   return results.sort((a: Record<string, number>, b: Record<string, number>) => {
-    return b.position - a.position;
+    return a.position - b.position;
   });
 });
