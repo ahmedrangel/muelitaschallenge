a8c20eb3255233936b289a176e70159d2d92bff8
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-19 15:39:09 -0500

    chore: handle null participants

--- server/api/participants.get.ts
+++ server/api/participants.get.ts
@@ -1,6 +1,8 @@
 export default defineEventHandler(async () => {
   const DB = process.env.PARTICIPANTS as any;
   const { results } = await DB.prepare("SELECT riot_name, riot_tag, lol_picture, twitch_login, twitch_display, twitch_picture, twitter, is_live, is_ingame, wins, losses, lp, elo, tier, position, position_change, instagram from participants").all();
+
+  if (!results[0]) return null;
   return results.sort((a: Record<string, number>, b: Record<string, number>) => {
     if (!a.position || !b.position) {
       if (!a.position) return 1; // Colocar a 'a' al final
