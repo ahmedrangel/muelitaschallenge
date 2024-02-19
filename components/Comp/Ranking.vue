4803def64f76e4c3fb0d083e60813ca28f3ac772
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-19 15:31:46 -0500

    chore: add border tr

--- components/Comp/Ranking.vue
+++ components/Comp/Ranking.vue
@@ -129,7 +129,7 @@ onBeforeUnmount(() => {
         </tr>
       </thead>
       <tbody>
-        <tr v-for="p of participants" :key="p.position" class="text-center align-middle">
+        <tr v-for="p of participants" :key="p.position" class="text-center align-middle border">
           <th scope="row" style="width: 20px;"><small>{{ p.position }}</small></th>
           <th scope="row" style="width: 40px;">
             <div class="d-flex align-items-center justify-content-center gap-1" :class="`${p.position_change > 0 ? 'text-positive' : p.position_change < 0 ? 'text-negative' : 'text-muted'}`">
