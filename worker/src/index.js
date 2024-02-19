b2804ee81439e8210f22983ec024cd9355afd517
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-19 18:28:07 -0500

    chore: update cron to each 10 min

--- worker/src/index.js
+++ worker/src/index.js
@@ -74,7 +74,7 @@ export default {
   },
   async scheduled(event, env) {
     switch (event.cron) {
-    case "*/20 * * * *":
+    case "*/10 * * * *":
       await updateGeneralData(env);
       break;
     }
