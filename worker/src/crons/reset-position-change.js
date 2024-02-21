764335d0d7eeac9ab80e1539289824032c1d1a14
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-21 04:03:07 -0500

    fix: position change cron

--- worker/src/crons/reset-position-change.js
+++ worker/src/crons/reset-position-change.js
@@ -1,8 +1,6 @@
 export const resetPositionChange = async(env) => {
-  const { results } = await env.PARTICIPANTS.prepare("SELECT id_summoner, position_change from participants").all();
-  for (const r of results) {
-    await env.PARTICIPANTS.prepare("UPDATE participants SET position_change = 0 WHERE id_summoner = ?").bind(r.id_summoner).run();
-  }
+  await env.PARTICIPANTS.prepare("UPDATE participants SET position_change = ? WHERE position_change > ?")
+    .bind(0, 0).run();
 
-  return results;
+  return "Reset";
 };
