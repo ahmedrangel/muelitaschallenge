75c9d0c1d802accb37d2f29bada840443625a931
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-21 04:54:30 -0500

    chore: add dev example

--- server/api/participants.get.ts
+++ server/api/participants.get.ts
@@ -1,4 +1,8 @@
+import example from "../../content/participants-example.json";
+
 export default defineEventHandler(async () => {
+  if (process.dev) return example;
+
   const DB = process.env.PARTICIPANTS as any;
   const { results } = await DB.prepare("SELECT riot_name, riot_tag, lol_picture, twitch_login, twitch_display, twitch_picture, twitter, is_live, is_ingame, wins, losses, lp, elo, tier, position, position_change, instagram from participants").all();
 
