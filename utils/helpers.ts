7348bdeda88dab5a918488f7afb3acb94d574a9b
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-22 17:34:26 -0500

    chore: update time display adjust

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -104,7 +104,7 @@ export const getTimeUnitsFromISODate = (ISO: string) => {
   if (dias > 0) unidades.push(`${dias} día${dias !== 1 ? "s" : ""}`);
   if (horas > 0) unidades.push(`${horas} hora${horas !== 1 ? "s" : ""}`);
   if (minutos > 0 && anios === 0) unidades.push(`${minutos} minuto${minutos !== 1 ? "s" : ""}`);
-  if (segundos > 0 && anios === 0 && meses === 0 && dias === 0 && horas === 0 && minutos === 0) unidades.push(`${segundos} segundo${segundos !== 1 ? "s" : ""}`);
+  if (segundos >= 0 && anios === 0 && meses === 0 && dias === 0 && horas === 0 && minutos === 0) unidades.push(`${segundos} segundo${segundos !== 1 ? "s" : ""}`);
 
   const result = unidades.join(", ");
   return result;
