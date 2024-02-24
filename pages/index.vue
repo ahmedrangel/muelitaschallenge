cc9121157d130137aa47c1008ea0cf279a3851c9
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-24 01:39:15 -0500

    chore: adjust

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@ const icon_version = data.value?.lol_icon_version;
     <CompRanking v-if="participants" :data="participants" :icon-version="icon_version" />
     <div class="justify-content-start align-items-center d-flex gap-1 small mt-1">
       <Icon name="ph:info-bold" />
-      <span>Los cambios de posición y partidas restantes se restauran a las 00:00 hora server (GMT-6).</span>
+      <span>Los cambios de posición y partidas restantes se restauran a las 00:00 <NuxtLink to="/">hora server</NuxtLink> (GMT-6).</span>
     </div>
   </main>
 </template>
