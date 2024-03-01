0fbe8338a7050d100cf62829b0dc31cdbb6856dc
Ahmed Rangel <ahmedrangel@outlook.com>
2024-03-01 18:40:29 -0500

    chore: show initial

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,7 +1,7 @@
 export const getPercentage = (wins: number, losses: number) => {
   const winrate = wins / (wins + losses) * 100;
   const formatted = winrate % 1 === 0 ? winrate.toFixed(0) : winrate.toFixed(1);
-  return formatted;
+  return wins && losses ? formatted : 0;
 };
 
 export const table_head = [
