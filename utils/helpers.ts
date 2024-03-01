5b52f5c493e0c988e531310837605ff14bca6231
Ahmed Rangel <ahmedrangel@outlook.com>
2024-03-01 18:44:45 -0500

    Update helpers.ts

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -2,6 +2,7 @@ export const getPercentage = (wins: number, losses: number) => {
   const winrate = wins / (wins + losses) * 100;
   const formatted = winrate % 1 === 0 ? winrate.toFixed(0) : winrate.toFixed(1);
   return formatted;
+}
 
 export const table_head = [
   {
