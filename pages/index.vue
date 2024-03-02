4318d989f4308f901093990d8d9bf534356eda01
Ahmed Rangel <ahmedrangel@outlook.com>
2024-03-01 23:49:48 -0500

    chore: add extra info

--- pages/index.vue
+++ pages/index.vue
@@ -37,9 +37,13 @@ useHead({
     <CompParticipantsCounter :data="participants" :last-updated="last_updated" />
     <!-- Tabla de clasificación -->
     <CompRanking v-if="participants" :data="participants" />
-    <div class="justify-content-start align-items-center d-flex gap-1 small mt-1">
-      <Icon name="ph:info-bold" />
-      <span>Los cambios de posición y partidas restantes se restauran a las 00:00 <NuxtLink to="/">hora server</NuxtLink> (GMT-6).</span>
+    <div class="justify-content-start align-items-center d-flex gap-2 small mt-1">
+      <Icon name="ph:info-bold" class="h4 mb-0" />
+      <div>
+        <span>La tabla se actualiza <b>cada 5 minutos</b>. Asegurate de refrescar la página para ver los últimos resultados</span>
+        <br>
+        <span>Los cambios de posición y partidas restantes se restauran a las 00:00 <NuxtLink to="/">hora server</NuxtLink> (GMT-6).</span>
+      </div>
     </div>
   </main>
 </template>
