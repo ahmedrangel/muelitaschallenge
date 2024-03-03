6d095fd67e83adf748edeb26bc155230e102c13a
Ahmed Rangel <ahmedrangel@outlook.com>
2024-03-03 01:07:37 -0500

    fix: update reset position change

--- worker/src/utils/helpers.js
+++ worker/src/utils/helpers.js
@@ -12,7 +12,7 @@ export const resetPositionChange = async(env) => {
   const nowInCDMX = new Date(now.toLocaleString("en-US", { timeZone }));
   const hours = nowInCDMX.getHours();
   const minutes = nowInCDMX.getMinutes();
-  if (hours === 0 && minutes < 10) {
+  if (hours === 0 && minutes < 5) {
     await env.PARTICIPANTS.prepare("UPDATE participants SET position_change = 0 WHERE position_change IS NOT 0").run();
     console.info("position change reseted");
   }
