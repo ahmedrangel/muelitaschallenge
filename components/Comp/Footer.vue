b8a41e5c7786e0f743aff76d7d3108a38cdec4c9
Ahmed Rangel <ahmedrangel@outlook.com>
2024-03-04 16:17:08 -0500

    chore: fw-bold to users

--- components/Comp/Footer.vue
+++ components/Comp/Footer.vue
@@ -4,7 +4,7 @@
     <h5 class="mb-2 small fw-light">
       Organizado por:
       <template v-for="(d, i) of SITE.organizers" :key="i">
-        <a target="_blank" :href="`https://twitch.com/${d.user}`" :style="{'color': '#' + d.color + '!important;' }">
+        <a target="_blank" class="fw-bold" :href="`https://twitch.com/${d.user}`" :style="{'color': '#' + d.color + '!important;' }">
           {{ d.name }}
         </a>{{ i < SITE.organizers.length - 1 ? ' & ' : '' }}
       </template>
@@ -12,7 +12,7 @@
     <h6 class="mb-2 small fw-light">
       Desarrollado por:
       <template v-for="(d, i) of SITE.developers" :key="i">
-        <a target="_blank" :href="`https://github.com/${d.github}`" :style="{'color': '#' + d.color + '!important;' }">
+        <a target="_blank" class="fw-bold" :href="`https://github.com/${d.github}`" :style="{'color': '#' + d.color + '!important;' }">
           {{ d.user }}
         </a>{{ i < SITE.developers.length - 1 ? ' & ' : '' }}
       </template>
@@ -20,7 +20,7 @@
     <h6 class="mb-0 small fw-light">
       Logo y banner por:
       <template v-for="(d, i) of SITE.designers" :key="i">
-        <a target="_blank" :href="`https://twitch.com/${d.user}`" :style="{'color': '#' + d.color + '!important;' }">
+        <a target="_blank" class="fw-bold" :href="`https://twitch.com/${d.user}`" :style="{'color': '#' + d.color + '!important;' }">
           {{ d.name }}
         </a>{{ i < SITE.designers.length - 1 ? ' & ' : '' }}
       </template>
