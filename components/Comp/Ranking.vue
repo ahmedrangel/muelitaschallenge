51e6baad3ecfad360ae026bda9bfb54354e77f25
Ahmed Rangel <ahmedrangel@outlook.com>
2024-04-03 01:40:27 -0500

    chore: add border winners

--- components/Comp/Ranking.vue
+++ components/Comp/Ranking.vue
@@ -144,7 +144,7 @@ const remainMatchesToday = (total: number) => {
           <td class="text-start">
             <div class="d-flex align-items-center gap-2 px-1">
               <div class="position-relative">
-                <img class="rounded-circle img-profile" :src="`https://static-cdn.jtvnw.net/${p.twitch_picture.replace('300x300', '70x70')}`">
+                <img class="rounded-circle img-profile" :class="`${p.position === 1 ? 'border-winner-1' : p.position === 2 ? 'border-winner-2' : p.position === 3 ? 'border-winner-3' : ''}`" :src="`https://static-cdn.jtvnw.net/${p.twitch_picture.replace('300x300', '70x70')}`">
                 <Icon v-if="p.position === 1" class="position-absolute top-0 start-50 translate-middle crown-1" name="ph:crown-fill" />
                 <Icon v-if="p.position === 2" class="position-absolute top-0 start-50 translate-middle crown-2" name="ph:crown-fill" />
                 <Icon v-if="p.position === 3" class="position-absolute top-0 start-50 translate-middle crown-3" name="ph:crown-fill" />
